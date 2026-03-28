f5e5be71bfc6962e4c907768983b190d60b1a7d7
suyashdhumal35 <suyashdhumal35@gmail.com>
2026-03-29 00:25:59 +0530

    api get and post

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,54 @@ require('dotenv').config();
 const express = require("express");
 const connectDB = require("./config/database")
 const app = express();
+const User = require("./models/User")
 
-connectDB().then(() => {
-    console.log("Connected to MongoDB..............");
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-        console.log(`http://localhost:${process.env.PORT}`);
+app.use(express.json());
 
-    })
+app.post("/signup", async (req, res) => {
+    try {
+        const user = new User(req.body)
+        await user.save();
+        res.send("User Added sucessfully!!!");
+    } catch (err) {
+        res.status(400).send("Error saving the user :" + err.message);
+    }
 })
+
+// get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.findOne({ emailId: userEmail })
+        if (!users) {
+            res.status(400).send("User not found ");
+        } else {
+            res.send(users)
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong!!!");
+    }
+})
+
+// feed api-get all the users from the database
+app.get("/feed", async (req, res) => {
+
+    try {
+        const users = await User.find()
+        res.send(users)
+    } catch (err) {
+        res.status(400).send("Something went wrong!!!");
+    }
+})
+
+connectDB()
+    .then(() => {
+        console.log("Connected to MongoDB..............");
+        app.listen(process.env.PORT, () => {
+            // console.log(`Server is running on port ${process.env.PORT}`);
+            console.log(`http://localhost:${process.env.PORT}`);
+        })
+    })
     .catch((err) => {
         console.error("Error conecting to MongoDB !!!!!!!!!!", err);
     })
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const userScheme = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     emailId: { type: String, required: true },
-    passsword: { type: String, required: true },
-    age: { type: Number, },
+    password: { type: String, required: true },
+    age: { type: Number },
     gender: { type: String }
-})
+});
 
-module.exorts = mongoose.model("User", userScheme)
+module.exports = mongoose.model("User", userSchema);
